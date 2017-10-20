f177f05078c4701f154d90448690c51ce522e9b5
Natalia Lyubova <nlyubova@aldebaran.com>
2017-10-20 14:47:45 +0200

    updating the menu

--- include/romeo_moveit_actions/tools.hpp
+++ include/romeo_moveit_actions/tools.hpp
@@ -22,100 +22,66 @@
 namespace moveit_simple_actions
 {
 
-  void printTutorial(const std::string robot_name)//, const int state
+  void printTutorial(const std::string robot_name)
   {
-    std::string key = "u";
-    //if (state == 0)
-    //{
-      std::cout <<
-                            "\n \n"
-                            << "************************************************\n"
-                            << "Tutorial for simple actions with Romeo, NAO, and Pepper robots \n"
-                            << "You have choosen -" << robot_name << "- robot."
-                            << std::endl;
+    std::cout << "\n \n"
+              << "************************************************\n"
+              << "Tutorial for simple actions with Romeo, NAO, and Pepper.\n"
+              << "You have chosen -" << robot_name << "- robot. \n\n"
 
-      std::cout <<
-                            "In your scene, you should see \n"
-                            << "- a robot \n"
-                            << "- a green cylinder -object-"
-                            << std::endl;
+              << "In your scene, you should see:\n"
+              << "- a robot\n"
+              << "- an object\n"
+              << "- [optionally] a table. \n\n"
 
-      if (robot_name == "romeo")
-      {
-        key = "g";
-        std::cout <<
-                            "- a black table."
-                            << std::endl;
-
-        std::cout <<
-                            "We inform you, that this tutorial works best with -Romeo- robot."
-                            << std::endl;;
-      }
-
-    //}
-    //else if (state == 1)
-      std::cout <<
-                            "\n The scenario is \n"
-                            << "1) Try to grasp an object (for Romeo), by pressing -" << key << "- key and enter.\n"
-                            << std::endl;
-    //else if (state == 3)
-      std::cout <<
-                            "2) If your grasp is sucessful, try to place the object, by typing -place-"
-                            << std::endl;
-
-    //else if (state == 2)
-       std::cout <<
-                            "   If your grasp is unsuccessfull, try to grasp again.\n"
-                             << "   Also, you can move the object right/left/top/down/farther/closer by pressing -s- -f- -e- -x- -r- -c- keys"
-                             << std::endl;
-
-    //else if (state == 4)
-      std::cout <<
-                            "3) Try to grasp with another hand"
-                            << std::endl;
-
-      std::cout <<
-      "************************************************\n"
-         << std::endl;
-
+              << "The scenario is\n"
+              << "1) Try to grasp an object (for Romeo only) by pressing -g- + Enter.\n"
+              << "2) If the grasp is successful, place the object by typing -place-\n"
+              << "   If not, try an approximative grasp by typing -u- + Enter.\n"
+              << "3) Try to move the object right/left/top/down/farther/closer\n"
+              << " by pressing -s- -f- -e- -x- -r- -c- keys and grasp again.\n"
+              << "4) Try to grasp with another hand.\n"
+              << "************************************************\n"
+              << std::endl;
   }
 
   void printAllActions()
   {
-    ROS_INFO_STREAM_NAMED("simple_actions:", "Possible actions are: \n"
-                          << " g - pick an object, \n"
-                          << " p - place the object, \n"
+    ROS_INFO_STREAM("Possible actions are: \n"
+                    << " g - pick an object, \n"
+                    << " p - place the object, \n"
 
-                          << " u - reach and grasp, \n"
-                          //<< " pregrasp - reach the pregrasp pose, \n"
-                          //<< " reachtop - reach the object from top, \n"
-                          //<< " w - reach the init pose, \n"
+                    << " u - reach and grasp, \n"
+                    //<< " pregrasp - reach the pregrasp pose, \n"
+                    //<< " reachtop - reach the object from top, \n"
 
-                          //<< " plan - plan grasping the object, \n"
-                          << " a - plan all possible grasps, \n"
-                          //<< " execute - execute the planned action, \n"
+                    //<< " plan - plan grasping the object, \n"
+                    << " a - plan all possible grasps, \n"
+                    //<< " execute - execute the planned action, \n"
 
-                          //<< " v - show the pregrasp pose, \n"
-                          << " i - go to init pose, \n" //(i0, i1, i2, i3)
-                          //<< " z - move the hand to the zero pose, \n"
-                          //<< " open - open hand, \n"
-                          //<< " close - close hand, \n"
-                          //<< " m - move the head to look down, \n"
+                    << " i - go to init pose (i1, i2, i3, i0), \n"
+                    //<< " v - show the pregrasp pose, \n"
+                    //<< " z - move the hand to the zero pose, \n"
+                    //<< " open - open hand, \n"
+                    //<< " close - close hand, \n"
+                    //<< " m - move the head to look down, \n"
 
-                          << " lc - add a cylinder on the left, \n"
-                          << " rc - add a cylinder on the right, \n"
-                          << " t - add/remove the table/scene, \n"
-                          << " d - detect objects, \n"
-                          //<< " n - process the next object, \n"
-                          << " e/s/f/x/r/c - move the object top/left/right/down/closer/farther \n"
+                    << " d - detect objects, \n"
+                    << " lc - add a cylinder on the left, \n"
+                    << " rc - add a cylinder on the right, \n"
+                    << " t - add/remove the table/scene, \n"
+                    //<< " n - process the next object, \n"
+                    << " e/s/f/x/r/c - move the object top/left/right/down/closer/farther \n"
 
-                          << " test_pick - test the goal space for picking, \n"
-                          << " test_reach - test the goal space for reaching, \n"
-                          //<< " stat - print statistics on successfull grasps, \n"
+                    //<< " switcharm - switch the active arm, \n"
 
-                          << " q - exit, \n"
-                          << " So, what do you choose?"
-                          );
+                    << " test_pick - test the goal space for picking, \n"
+                    << " test_reach - test the goal space for reaching, \n"
+                    //<< " stat - print statistics on successfull grasps, \n"
+
+                    << " q - exit, \n"
+                    << " So, what do you choose?"
+                    );
   }
 
   int promptUserAction()
@@ -147,6 +113,14 @@ namespace moveit_simple_actions
   }
 
   double promptUserValue(const std::string command)
+  {
+    ROS_INFO_STREAM_NAMED("pick_place",command);
+    double input;
+    std::cin >> input;
+    return input;
+  }
+
+  double promptUserInt(const std::string command)
   {
     ROS_INFO_STREAM_NAMED("pick_place",command);
     double input;
